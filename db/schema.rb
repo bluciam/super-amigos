c320e447421af5a0a147d6dd17c028e5878bdc2b
Blanca Mancilla <blancalmancilla@gmail.com>
2016-11-13 18:15:37 -0500

    Ready for Heroku modified:   Gemfile modified:   Gemfile.lock renamed:    app/assets/stylesheets/application.css -> app/assets/stylesheets/application.css.sass modified:   app/assets/stylesheets/contacts.scss modified:   app/assets/stylesheets/scaffolds.scss modified:   app/controllers/application_controller.rb modified:   app/controllers/contacts_controller.rb modified:   app/views/contacts/_form.html.erb modified:   app/views/contacts/edit.html.erb modified:   app/views/contacts/index.html.erb modified:   app/views/contacts/new.html.erb modified:   app/views/contacts/show.html.erb modified:   app/views/welcome/index.html.erb modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160821233549) do
+ActiveRecord::Schema.define(version: 20161113033019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160821233549) do
     t.string   "mobile"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "email"
   end
 
 end
